502b6b41a00e422f26b07f53937b25cbdae9e43c
Sutanshu <sutanshu@ualberta.ca>
2023-02-19 21:21:46 -0700

    f

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -1,5 +1,23 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        justifyItems: "center",
+        height: "10vh",
+        width: "100vw",
+        backgroundColor: "white",
+      }}
+    >
+      <h1 style={{ color: "green", background: "black" }}>Sutanshu Seth</h1>
+      <h1> Another one </h1>
+    </Box>
+  );
 }
 
 export default App;
