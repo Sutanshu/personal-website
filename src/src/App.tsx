40394c7f2a9b4c179c994f46f6158206e5d9b2f2
Sutanshu Seth <sutanshu@ualberta.ca>
2023-02-28 15:02:49 -0700

    feat: navbar additions

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -14,18 +14,6 @@ function App() {
   const ref = React.useRef<HTMLInputElement>(null);
   const [selected, setSelected] = React.useState(true);
 
-  const search = () => {
-    setSelected(!selected);
-
-    if (ref && ref.current && selected) {
-      ref.current.style.transform = "translateY(-40vh)";
-      ref.current.style.transition = "transform 1s";
-    }
-    if (ref && ref.current && !selected) {
-      ref.current.style.transform = "translateY(0vh)";
-    }
-  };
-
   const getPDF = () => {
     return (
       <Box
@@ -48,106 +36,7 @@ function App() {
     );
   };
 
-  const getSearchEngine = () => {
-    return (
-      <Box
-        sx={{
-          height: "90vh",
-          backgroundColor: "white",
-          justifyContent: "center",
-          alignItems: "flex-start",
-          display: "flex",
-          flexDirection: "column",
-        }}
-      >
-        <Box
-          ref={ref}
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "normal",
-          }}
-        >
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "space-between",
-              // justifyItems: "center",
-            }}
-          >
-            <Button
-              sx={{
-                ":hover": {
-                  backgroundColor: "#3492CB",
-                  color: "white",
-                },
-              }}
-            >
-              Google
-            </Button>
-            <Button
-              sx={{
-                ":hover": {
-                  backgroundColor: "#3492CB",
-                  color: "white",
-                },
-              }}
-            >
-              Bing
-            </Button>
-            <Button
-              sx={{
-                ":hover": {
-                  backgroundColor: "#3492CB",
-                  color: "white",
-                },
-              }}
-            >
-              DuckDuckGo
-            </Button>
-          </div>
-          <Box
-            ref={ref}
-            sx={{
-              height: "10vh",
-              width: "40vw",
-              backgroundColor: "white",
-              justifyContent: "center",
-              alignItems: "center",
-              display: "flex",
-              borderColor: "black",
-              borderRadius: "10px",
-              transition: "transform 1s",
-            }}
-          >
-            <TextField
-              id="standard-basic"
-              label="Search Engine"
-              variant="standard"
-              sx={{ width: "60%" }}
-              onChange={(e) => setQuery(e.target.value)}
-              onKeyDown={(e) => e.key === "Enter" && search()}
-              value={query}
-            ></TextField>
-            <SearchIcon
-              sx={{
-                color: "#3492CB",
-                paddingRight: "10px",
-                ":hover": {
-                  color: "#1913A0",
-                  cursor: "pointer",
-                },
-              }}
-              onClick={search}
-            />
-          </Box>
-        </Box>
-      </Box>
-    );
-  };
-
-  const getExperience = () => {
+  const Experience = () => {
     return (
       <Box
         sx={{
@@ -167,7 +56,7 @@ function App() {
     );
   };
 
-  const getSkills = () => {
+  const Skills = () => {
     return (
       <Box
         sx={{
@@ -183,6 +72,101 @@ function App() {
         }}
       >
         <h1 style={{ color: "black" }}>Skills</h1>
+      </Box>
+    );
+  };
+
+  const NavBar = () => {
+    return (
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          justifyItems: "center",
+          justifyContent: "space-between",
+          height: "10vh",
+          width: "100vw",
+          backgroundColor: "white",
+        }}
+      >
+        <h1>Sutanshu Seth</h1>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            justifyItems: "center",
+            alignItems: "center",
+            paddingTop: "2vh",
+            height: "5vh",
+          }}
+        >
+          <Button
+            sx={{
+              ":hover": {
+                backgroundColor: "#3492CB",
+                color: "white",
+              },
+              color: "black",
+            }}
+          >
+            Resume
+          </Button>
+          <Button
+            sx={{
+              ":hover": {
+                backgroundColor: "#3492CB",
+                color: "white",
+              },
+              color: "black",
+            }}
+          >
+            Github
+          </Button>
+          <Button
+            sx={{
+              ":hover": {
+                backgroundColor: "#3492CB",
+                color: "white",
+              },
+              color: "black",
+            }}
+          >
+            LinkedIn
+          </Button>
+        </div>
+      </Box>
+    );
+  };
+  const HomePage = (): JSX.Element => {
+    return (
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "normal",
+          height: "100vh",
+          width: "100vw",
+          backgroundColor: "black",
+          border: "1px solid black",
+        }}
+      >
+        <NavBar />
+        <Box
+          sx={{
+            height: "100vh",
+            backgroundColor: "white",
+            justifyContent: "center",
+            alignItems: "center",
+            display: "flex",
+            flexDirection: "column",
+            color: "black",
+          }}
+        >
+          <h1 ref={ref} style={{ color: "black" }}>
+            Home
+          </h1>
+        </Box>
       </Box>
     );
   };
@@ -224,34 +208,9 @@ function App() {
   }, [ref, observer]);
   return (
     <>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "normal",
-          height: "100vh",
-          width: "100vw",
-          backgroundColor: "black",
-        }}
-      >
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-between",
-            justifyItems: "center",
-            height: "10vh",
-            width: "100vw",
-            backgroundColor: "white",
-          }}
-        >
-          <h1>Sutanshu Seth</h1>
-        </Box>
-      </Box>
-      {/* {getSearchEngine()} */}
-      {/* {getPDF()} */}
-      {getExperience()}
-      {getSkills()}
+      <HomePage />
+      <Experience />
+      <Skills />
     </>
   );
 }
